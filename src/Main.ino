9a1790fdde90727bae2effb37ac62123ae8b32af
Edrig <contact@bobbycorp.eu>
2017-03-08 23:31:07 +0100

    first Commit

---
+++ src/Main.ino
@@ -0,0 +1,42 @@
+
+// BP choix couleur
+#define Bp_R //Bp choix rose
+#define Bp_B //Bp choix bleu
+
+// Inverseur choix mode
+#define IntF_R //Inter force mode rose
+#define IntF_B //Inter force mode bleu
+
+// ILS
+#define Ils_R  //loco stop remise
+#define Ils_P  //loco stop pont-secteur
+#define Ils_AB //loco stop voie AB plaque
+#define Ils_C  //loco stop voie C plaque
+
+// Contact
+#define Cto_R // porte remise open
+#define Ctc_R // porte remise close
+
+#define Cto_U // porte usine open
+#define Ctc_U // porte usine close
+
+#define Cto_B // barriere cour open
+#define Ctc_B // barriere cour close
+
+#define Aig_A // aiguille voie A
+#define Aig_B // aiguille voie B
+
+#define Pts_B // secteur voie B
+#define Pts_C // secteur voie C
+#define Pts_D // secteur voie D
+
+void setup()
+{
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
