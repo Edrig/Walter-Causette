f9ca0bc70d3689ee3fb7af34038367256c9e6a46
[Bobbycorp] Edrig <[Bobbycorp] Edrig>
2017-09-18 22:15:04 +0200

    refactor Signed-off-by: [Bobbycorp] Edrig <[Bobbycorp] Edrig>

--- Maitre/src/Main/Main.ino
+++ Maitre/src/Main/Main.ino
@@ -7,8 +7,8 @@
 
 //------ BP choix couleur ------//
 
-PushButton Bp_R = PushButton(A0);  //Bp choix rose
-PushButton Bp_B = PushButton(A1);   //Bp choix bleu
+PushButton Bp_R = PushButton(2);  //Bp choix rose
+PushButton Bp_B = PushButton(3);   //Bp choix bleu
 
 //------ Inverseur mode ------//
 #define IntF_R //Inter force mode rose
@@ -16,10 +16,10 @@ PushButton Bp_B = PushButton(A1);   //Bp choix bleu
 
 //------ Ils ------//
 
-PushButton Ils_R = PushButton(A2);  //loco stop remise
-PushButton Ils_P = PushButton(A3); //loco stop pont-secteur
-PushButton Ils_AB = PushButton(A4); //loco stop voie AB plaque
-PushButton Ils_C = PushButton(A5); //loco stop voie C plaque
+PushButton Ils_R = PushButton(4);  //loco stop remise
+PushButton Ils_P = PushButton(5); //loco stop pont-secteur
+PushButton Ils_AB = PushButton(6); //loco stop voie AB plaque
+PushButton Ils_C = PushButton(7); //loco stop voie C plaque
 
 //------ Contact ------//
 
@@ -31,26 +31,26 @@ int Lamp = A1;
 //BruitU = ;
 
 // Moment
-int porteR = 4;
-int porteU = 5;
+int porteR = A2;
+int porteU = A3;
 
-int barriere = 6;
+int barriere = A4;
 
-int cmdAigA = 7;
-int cmdAigB = 8;
+int cmdAigA = 8;
+int cmdAigB = 9;
 
-int cmdSectB = 9;
-int cmdSectC = 10;
+int cmdSectB = 10;
+int cmdSectC = 11;
 
-int AlimVL = 11;
-int AlimVR = 12;
+int AlimVL = 12;
+int AlimVR = 13;
 
 //------ Actionneur ------//
 
 
 //------ Led ------//
-Blinker BkR(2); // clignotement Led Rose
-Blinker BkB(3); // clignotement Led Bleu
+Blinker BkR(5); // clignotement Led Rose
+Blinker BkB(6); // clignotement Led Bleu
 
 //------ Boolean ------//
 boolean Cmd = false; // Choix couleur
@@ -91,13 +91,10 @@ void setup()
   //Pt Voie C
   //Aig Voie A
   //Ps voie C
-
-  pinMode(AlimVL, OUTPUT);
-  pinMode(AlimVR, OUTPUT);
-
   AlimV(zero);
 
-  //digitalWrite(AlimVL, HIGH);
+
+
   t = 0;
 
 }
@@ -119,147 +116,117 @@ void loop()
   switch (t)
   {
     case 1:
-      Serial.println("case 1");
       //Led fade out
 
       delay(2000);
       t++;
       break;
     case 2:
-      Serial.println("case 2");
       digitalWrite(Lamp, HIGH);
       t++;
       break;
     case 3:
-      Serial.println("case 3");
       //BMoteur Ralentie
       Bp(porteR);
       delay(1000);
       t++;
       break;
     case 4:
-    Serial.println("case 4");
       //BMoteur Demarrage
       AlimV(right);
       break;
     case 5:
-    Serial.println("case 5");
       //Bmoteur Ralentie
       AlimV(zero);
       delay(2000);
       t++;
       break;
     case 6:
-    Serial.println("case 6");
       //Led Fade in
       //Pt C+25°
       t++;
       break;
     case 7:
-    Serial.println("case 7");
       Bp(barriere);
       delay(2000);
       t++;
       break;
     case 8:
-    Serial.println("case 8");
       Bp(cmdAigB);
       delay(2000);
       t++;
       break;
     case 9:
-    Serial.println("case 9");
       //Attente position Pt
       //BMoteur Demarrage
       AlimV(left);
       break;
     case 10:
-    Serial.println("case 10");
       //Bmoteur Ralentie
       AlimV(zero);
       t++;
       delay(2000);
       break;
     case 11:
-    Serial.println("case 11");
       //Pt AB-25°
       t++;
       break;
     case 12:
-    Serial.println("case 12");
       Bp(porteU);
       break;
     case 13:
-    Serial.println("case 13");
       //Attente position Pt
       //BMoteur Demarrage
       break;
     case 14:
-    Serial.println("case 14");
 
       break;
     case 15:
-    Serial.println("case 15");
 
       break;
     case 16:
-    Serial.println("case 16");
 
       break;
     case 17:
-    Serial.println("case 17");
 
       break;
     case 18:
-    Serial.println("case 18");
 
       break;
     case 19:
-    Serial.println("case 19");
 
       break;
     case 20:
-    Serial.println("case 20");
 
       break;
     case 21:
-    Serial.println("case 21");
 
       break;
     case 22:
-    Serial.println("case 22");
 
       break;
     case 23:
-    Serial.println("case 23");
 
       break;
     case 24:
-    Serial.println("case 24");
 
       break;
     case 25:
-    Serial.println("case 25");
 
       break;
     case 26:
-    Serial.println("case 26");
 
       break;
     case 27:
-    Serial.println("case 27");
 
       break;
     case 28:
-    Serial.println("case 28");
 
       break;
     case 29:
-    Serial.println("case 29");
 
       break;
     case 30:
-    Serial.println("case 30");
 
       break;
   }
@@ -305,24 +272,22 @@ void onBp_BPressed(Button& btn)
 
 void onIls_R_Pressed(Button& btn)
 {
-  Serial.println("Ils loco-Remise");
+
 }
 
 void onIls_P_Pressed(Button& btn)
 {
-  Serial.println("Ils pont-secteur");
   switch (t)
   {
     case 9:
       t++;
       break;
   }
-}1
+}
 
 
 void onIls_AB_Pressed(Button& btn)
 {
-  Serial.println("Ils loco-AB");
   switch (t)
   {
     case 4:
@@ -333,12 +298,11 @@ void onIls_AB_Pressed(Button& btn)
 
 void onIls_C_Pressed(Button& btn)
 {
-  Serial.println("Ils loco-C");
+
 }
 
 void Bp(int s)
 {
-  Serial.println("BP: "+ s);
   digitalWrite(s, HIGH);
   delay(60);
   digitalWrite(s, LOW);
@@ -349,20 +313,18 @@ void AlimV(AlimVoie v)
   switch (v)
   {
     case left:
-      Serial.println("AlimVoie: Left");
       digitalWrite(AlimVL, HIGH);
       digitalWrite(AlimVR, LOW);
       break;
     case zero:
-      Serial.println("AlimVoie: Zero");
       digitalWrite(AlimVL, LOW);
       digitalWrite(AlimVR, LOW);
       break;
     case right:
-      Serial.println("AlimVoie: Right");
       digitalWrite(AlimVL, LOW);
       digitalWrite(AlimVR, HIGH);
       break;
   }
 
 }
+
